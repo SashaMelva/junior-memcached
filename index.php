599036cf1e16eae5b29e824e6e1ba9787e3963c1
SashaMelva <sa1524517@gmail.com>
2023-03-27 13:31:23 +0300

    memcached

--- index.php
+++ index.php
@@ -1,43 +1,59 @@
 <?php
+session_start();
 $memcached = new Memcached();
 $memcached->addServer('jm-memcached1', 11211);
 $memcached->addServer('jm-memcached2', 11211);
 $memcached->addServer('jm-memcached3', 11211);
-print_r($memcached->getResultMessage());
-print_r('<hr />');
+
 
 $num = getNum($_SERVER['REQUEST_URI']);
-$var_key = $memcached->get("{$num}");
+$user = session_id();
+
+var_dump(session_id(), session_id());
+$var_key = $memcached->get("{$num}-{$user}");
+
 
 if (!empty($var_key)) {
     //Если объект закэширован, выводим его значение
     echo $var_key;
 } else {
+    var_dump('recalculate value');
     //Если в кэше нет объекта с ключом our_var, создадим его
     //Объект our_var будет храниться 5 минут и не будет сжат
     $fib = fib($num);
-    $memcached->set("{$num}", $fib, 300);
+    $memcached->set("{$num}-{$user}", $fib, 300);
     //Выведем закэшированные данные
     echo $fib;
 }
 
-function fib(int $a): int
+function fib(int $a)
 {
+    static $catch = [];
+
+    if(!empty($catch[$a])) {
+        return $catch[$a];
+    }
 
     if ($a === 0) {
-        return 0;
+        return $catch[$a] = 0;
     }
 
     if ($a === 1) {
-        return 1;
+        return $catch[$a] = 1;
     }
 
-    $result = fib($a - 1) + fib($a - 2);
-    return $result;
+    $catch[$a] = fib($a - 1) + fib($a - 2);
+    return  $catch[$a];
 }
 
 function getNum($url)
 {
-    $num = explode('=', $url);
-    return $num[1];
+    $data = explode('=', $url);
+    $num = explode('&', $data[1]);
+    return $num[0];
+}
+
+function getUser($url) {
+    $user = explode('=', $url);
+    return $user[2];
 }
